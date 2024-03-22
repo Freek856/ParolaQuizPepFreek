2dc7e7fff7a2d3686463741f77ecc259ce87c5ca
Freek856 <f.vandreumel@student.han.nl>
2024-03-22 11:16:25 +0100

    spreekt voor zichtzelf

--- src/main/java/ln/han/Main.java
+++ src/main/java/ln/han/Main.java
@@ -2,6 +2,6 @@ package ln.han;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Whoever threw that paper... your moms a hoe!");
     }
 }
